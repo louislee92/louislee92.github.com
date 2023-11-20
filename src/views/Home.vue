9bf1256f20d9d30df14bd4c9f115d9760bd290d8
Louis Lee <louislee92@163.com>
2023-11-20 17:50:04 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,7 +41,7 @@ export default {
       else if(id == 'game') {
         // this.$router.push({path: '/game'});
         // window.location.href = "http://game.xstx.fun"
-        window.location.href = "http://xstx.fun/AssetBundles/Android/gtl-0.6.0%20.apk";
+        window.location.href = "http://" + window.location.host + "/AssetBundles/Android/gtl-0.6.0.apk";
         // 跳转到
       }
       else if(id == "work") {
